cd610d2c6d9c4c8d063379d475f748e09f335d62
Aismita Sengupta <aismitasengupta@gmail.com>
2025-12-27 19:10:00 +0530

    Main workflow file

--- src/app.js
+++ src/app.js
@@ -1,4 +1,9 @@
 import express from "express"
+import cors from "cors"
+import healthCheckRouter from "./routes/healthcheck.routes.js";
+import userRouter from "./routes/user.routes.js"
+
+
 
 const app = express();
 
@@ -13,6 +18,19 @@ app.get("/", (req,res)=>{
 app.use(express.json("limit: 16kb")) //req body
 app.use(express.urlencoded({extended:true,limit: "16kb"})) //form data
 app.use(express.static("public") ) //static files
+
+//cors configuration
+app.use(cors({
+    origin: process.env.CORS_ORIGIN?.split(",") || "https://localhost:5173/",
+    credentials: true,
+    methods: ["GET", "POST", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Authorization", "Content-Type"]
+}))
+
+
+
+app.use("/api/v1/healthcheck", healthCheckRouter)
+app.use("/api/v1/auth", userRouter)
 
 
 export default app
