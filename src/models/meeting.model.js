012f773a1cf3fb0727ff005a083e40d35bc7f3f2
Aismita Sengupta <aismitasengupta@gmail.com>
2025-12-27 17:57:46 +0530

    Meeting model added

---
+++ src/models/meeting.model.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose"
+
+const meetingSchema = new mongoose.Schema({
+    user_id:{
+        type:mongoose.Schema.Types.ObjectId,
+        required: true,
+        lowercase: true,
+
+    },
+    meetingCode:{
+        type:String,
+        required:true,
+        trim:true,
+        unique:true
+    },
+    date:{
+        type:Date,
+        default: Date.now(),
+        required: true
+
+    }
+},{timestamps: true})
+
+export const Meeting = mongoose.model("Meeting",meetingSchema )
