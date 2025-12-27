1d6f69ab0c7d30c2ddf6414f194c0bef6988c721
Aismita Sengupta <aismitasengupta@gmail.com>
2025-12-27 17:55:32 +0530

    User model created

---
+++ src/models/user.model.js
@@ -0,0 +1,42 @@
+import mongoose from "mongoose"
+import bcrypt from "bcrypt"
+
+const UserSchema = new mongoose.Schema({
+    username: {
+        type:String,
+        required: true, 
+        unique:true,
+        index: true,
+        trim: true,
+        lowercase: true
+    },
+    email:{
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+         trim: true,
+    }, 
+      fullName:{
+        type: String,
+        trim: true
+      },
+    password:{
+        type: String,
+        required: [true, "Password is required!"],
+        select:false
+    },
+    isEmailVerified:{
+        type: Boolean,
+        default: false
+      }
+}, {timestamps:true})
+
+UserSchema.pre("save", async function () {
+  if (!this.isModified("password")) return
+  this.password = await bcrypt.hash(this.password, 10)
+})
+
+
+export const User = mongoose.model("User", UserSchema)
+
